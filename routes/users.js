e5a934df99bc11195864182a8db9b80e83689344
rscottlundgren <rscottlundgren@gmail.com>
2020-09-01 12:50:00 -0400

    Updated Authentication - 1. Created a more secure "POST" request. 2. Created a process to salt the password. 3. Created "Error" checks with express-validator (name, email, pw). 4. Created a process to hash the password. 5. Saved the user to the database.

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,55 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
+const { check, validationResult } = require('express-validator')
+
+const User = require('../models/user')
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post('/', (req, res) => {
-	res.send('Registers A User')
-})
+router.post(
+	'/',
+	[
+		check('name', 'Name Is Required.').not().isEmpty(),
+		check('email', 'Please Include A Valid Email.').isEmail(),
+		check(
+			'password',
+			'Please Enter A Password With Six (6) Or More Characters.'
+		).isLength({ min: 6 }),
+	],
+	async (req, res) => {
+		const errors = validationResult(req)
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() })
+		}
+
+		const { name, email, password } = req.body
+		try {
+			let user = await User.findOne({ email: email })
+
+			if (user) {
+				return res.status(400).json({ msg: 'User Already Exists.' })
+			}
+
+			user = new User({
+				name: name,
+				email: email,
+				password: password,
+			})
+
+			const salt = await bcrypt.genSalt(10)
+
+			user.password = await bcrypt.hash(password, salt)
+
+			await user.save()
+
+			res.send('User Saved!')
+		} catch (err) {
+			console.error(err.message)
+			res.status(500).send('Server Error.')
+		}
+	}
+)
 
 module.exports = router
