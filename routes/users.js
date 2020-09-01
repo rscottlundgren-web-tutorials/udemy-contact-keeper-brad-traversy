3fac4d702c7d9788bff8effd1c27316b78cff144
rscottlundgren <rscottlundgren@gmail.com>
2020-09-01 16:29:51 -0400

    Updated Authentication/Security - 1. Included JSON Web Tokens for secure authentication.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const { check, validationResult } = require('express-validator')
 
-const User = require('../models/user')
+const User = require('../models/User')
 
 // @route   POST api/users
 // @desc    Register a user
@@ -44,7 +46,23 @@ router.post(
 
 			await user.save()
 
-			res.send('User Saved!')
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			}
+
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{
+					expiresIn: 360000,
+				},
+				(err, token) => {
+					if (err) throw err
+					res.json({ token: token })
+				}
+			)
 		} catch (err) {
 			console.error(err.message)
 			res.status(500).send('Server Error.')
