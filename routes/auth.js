cbef2eaf8402e6de3404dad13002ad681867ceed
R. Scott Lundgren <rscottlundgren@gmail.com>
2022-03-12 17:19:44 -0500

    Add Template Routes To 'auth.js'
    - Created a scaffold GET request route to collect a logged in user.
    - Created a scaffold POST request route to authenticate a user.

--- routes/auth.js
+++ routes/auth.js
@@ -1,2 +1,21 @@
 // Will contain the following routes - Login, Authentication,
 // and a check of the login user
+
+const express = require('express');
+const router = express.Router();
+
+// @route     GET api/auth
+// @desc      Get Logged In User
+// @access    Private
+router.get('/', (req, res) => {
+	res.send('Get A Logged In User');
+});
+
+// @route     POST api/users
+// @desc      Auth User & Get Token
+// @access    Public
+router.post('/', (req, res) => {
+	res.send('Authenticate User');
+});
+
+module.exports = router;
