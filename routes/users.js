d056f24a3537785a31dc6f1c20001943e4d4542b
R. Scott Lundgren <rscottlundgren@gmail.com>
2022-03-12 19:57:30 -0500

    Update `users.js` To Validate Data Inputs
    - Imported `express-validator`.
    - Updated POST route to validate data inputs for registration fields.

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,30 @@
 // Will contain the "Register" route
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 // @route     POST api/users
 // @desc      Register A User
 // @access    Public
-router.post('/', (req, res) => {
-	res.send('Register A User');
-});
+router.post(
+	'/',
+	[
+		check('name', 'Please add a name.').not().isEmpty(),
+		check('email', 'Please include a valid email.').isEmail(),
+		check(
+			'password',
+			'Please enter a password with 6 or more characters.'
+		).isLength({ min: 6 }),
+	],
+	(req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		res.send('passed')
+	}
+);
 
 module.exports = router;
