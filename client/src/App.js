55493b1d3ed107afe406c10996fba5df8d6b4492
R. Scott Lundgren <rscottlundgren@gmail.com>
2022-03-19 17:28:54 -0400

    Import `Navbar` Component In `App.js`
    - Imported `Navbar` component into `App.js`
    - Added `Navbar` to arrow function

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from './components/layout/Navbar'
 import './App.css';
 
 const = App = () => {
 	return (
 		<div className='App'>
+			<Navbar />
       My Application
 		</div>
 	);
