8b0847644e44fdbb6733627110de52bd99b4a18a
R. Scott Lundgren <rscottlundgren@gmail.com>
2022-03-19 12:39:12 -0400

    Update `contacts.js` To Use `Contact.js` Model
    - Add requirements including `User`, `Contact` and `middleware/auth`
    - Updated GET route to retrieve a list of User's contacts in date order
    - Updated GET route so it's protected by `middleware/auth`

--- routes/contacts.js
+++ routes/contacts.js
@@ -4,12 +4,25 @@
 
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // @route     GET api/contacts
 // @desc      Get All User's Contacts
 // @access    Private
-router.get('/', (req, res) => {
-	res.send('Get A Users Contacts');
+router.get('/', auth, async (req, res) => {
+	try {
+		const contacts = await Contact.find({ user: req.user.id }).sort({
+			date: -1,
+		});
+		res.json(contacts);
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).send('Server Error');
+	}
 });
 
 // @route     POST api/contacts
